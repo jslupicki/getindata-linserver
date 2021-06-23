19dc500df0ef8229d4318385030fefaa10de29b6
jslupicki <YwUyezN3>
2021-06-23 10:44:53 +0200

    Start work on fast/slow path in search for memory constrain situations

--- src/main/java/com/slupicki/linserver/SearchService.java
+++ src/main/java/com/slupicki/linserver/SearchService.java
@@ -2,7 +2,6 @@ package com.slupicki.linserver;
 
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -18,8 +17,8 @@ public class SearchService {
     public static final Locale LOCALE_PL = new Locale("pl", "Pl");
 
     private String[] lines;
-
     private Map<String, Set<Integer>> index = Maps.newHashMap();
+    private int maxPhraseLengthToIndex = -1;
 
     public String getLine(int line) {
         if (line < 1 || line > lines.length) {
@@ -29,14 +28,21 @@ public class SearchService {
     }
 
     public String search(String phrase) {
-        String normalizedPhrase = String.join(" ", normalizeAndSplit(phrase));
-        log.info("Search for '%s' -> '%s'".formatted(phrase, normalizedPhrase));
-        if (!index.containsKey(normalizedPhrase)) {
+        String[] normalizedPhraseArray = normalizeAndSplit(phrase);
+        if (normalizedPhraseArray.length <= maxPhraseLengthToIndex) {
+            // Fast path
+            String normalizedPhrase = String.join(" ", normalizedPhraseArray);
+            log.info("Search for '%s' -> '%s'".formatted(phrase, normalizedPhrase));
+            if (!index.containsKey(normalizedPhrase)) {
+                throw new NotFoundException();
+            }
+            return index.get(normalizedPhrase).stream()
+                    .map(lineNumber -> lines[lineNumber])
+                    .collect(Collectors.joining("\n"));
+        } else {
+            // Slow path
             throw new NotFoundException();
         }
-        return index.get(normalizedPhrase).stream()
-                .map(lineNumber -> lines[lineNumber])
-                .collect(Collectors.joining("\n"));
     }
 
     public String[] getLines() {
@@ -51,6 +57,14 @@ public class SearchService {
         return index;
     }
 
+    public int getMaxPhraseLengthToIndex() {
+        return maxPhraseLengthToIndex;
+    }
+
+    public void setMaxPhraseLengthToIndex(int maxPhraseLengthToIndex) {
+        this.maxPhraseLengthToIndex = maxPhraseLengthToIndex;
+    }
+
     public void index() {
         index.clear();
         ArrayList<String[]> normalizedLines = new ArrayList<>();
@@ -60,20 +74,24 @@ public class SearchService {
             normalizedLines.add(normalizedLine);
             longestPhrase = Math.max(longestPhrase, normalizedLine.length);
         }
-        for (int phraseLength = 1; phraseLength <= longestPhrase; phraseLength++) {
+        maxPhraseLengthToIndex = maxPhraseLengthToIndex < 0 ? longestPhrase : maxPhraseLengthToIndex;
+        for (int phraseLength = 1; phraseLength <= maxPhraseLengthToIndex; phraseLength++) {
             for (int lineNumber = 0; lineNumber < normalizedLines.size(); lineNumber++) {
                 indexPhrase(normalizedLines.get(lineNumber), lineNumber, phraseLength);
             }
-            log.info("Indexed phrases of length {}. The longest phrase is {}", phraseLength, longestPhrase);
+            log.info("Indexed phrases of length {}. The longest phrase is {}. Max indexed phrase will be {}", phraseLength, longestPhrase, maxPhraseLengthToIndex);
         }
     }
 
     private void indexPhrase(String[] normalizedLine, int lineNumber, int phraseLength) {
         for(int wordIndex = 0; wordIndex < normalizedLine.length - phraseLength + 1; wordIndex++) {
             String[] phraseTable = new String[phraseLength];
+/*
             for(int i = 0; i < phraseLength; i++) {
                 phraseTable[i] = normalizedLine[wordIndex + i];
             }
+*/
+            System.arraycopy(normalizedLine, wordIndex, phraseTable, 0, phraseLength);
             String phrase = String.join(" ",phraseTable);
             if (!index.containsKey(phrase)) {
                 index.put(phrase, Sets.newHashSet());
@@ -83,7 +101,7 @@ public class SearchService {
     }
 
     private String[] normalizeAndSplit(String line) {
-        return line.toLowerCase(LOCALE_PL).split("\\P{L}+");
+        return line.toLowerCase(LOCALE_PL).strip().split("\\P{L}+", -1);
     }
 
 }
