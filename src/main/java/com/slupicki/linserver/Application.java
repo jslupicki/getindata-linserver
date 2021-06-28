c4b81fab12d5b436c4d47aee1f072ac84f9d8c51
jslupicki <YwUyezN3>
2021-06-28 08:33:16 +0200

    Add parse of input parameters and options

--- src/main/java/com/slupicki/linserver/Application.java
+++ src/main/java/com/slupicki/linserver/Application.java
@@ -1,5 +1,6 @@
 package com.slupicki.linserver;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
@@ -11,15 +12,17 @@ import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 @EnableAsync
 public class Application implements ApplicationRunner {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
+    public static final String LIMIT_OPTION = "limit";
+    public static final String DEFAULT_INPUT_FILE = "test.txt";
 
     private final SearchService searchService;
 
@@ -34,18 +37,22 @@ public class Application implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("Current directory: {}", System.getProperty("user.dir"));
-        String fileName = "test.txt";
+        String fileName = DEFAULT_INPUT_FILE;
+        validateArgs(args);
         if (args.getNonOptionArgs().isEmpty()) {
             log.info("No args - use default file '{}'", fileName);
         } else {
             fileName = args.getNonOptionArgs().get(0);
         }
-        Path path = Paths.get(fileName);
-        String[] lines = Files.lines(path).toArray(String[]::new);
-        searchService.setLines(lines);
-        log.info("Readed {} lines from '{}'", lines.length, fileName);
-        searchService.index();
-        log.info("Indexed {} phrases", searchService.getIndex().keySet().size());
+        List<String> limit = args.getOptionValues(LIMIT_OPTION);
+        if (limit == null) {
+            log.info("'-limit' option not set - will be no limit to length of indexed phrases");
+        } else {
+            int maxPhraseLength = Integer.parseInt(limit.get(0));
+            searchService.setMaxPhraseLengthToIndex(maxPhraseLength);
+            log.info("Maximum length indexed phrases is set to {}", maxPhraseLength);
+        }
+        searchService.loadFile(fileName);
     }
 
     @Bean()
@@ -56,5 +63,52 @@ public class Application implements ApplicationRunner {
         executor.setWaitForTasksToCompleteOnShutdown(true);
         executor.setThreadNamePrefix("Async-");
         return executor;
+    }
+
+    private void validateArgs(ApplicationArguments args) {
+        List<String> nonOptionArgs = args.getNonOptionArgs();
+        Set<String> optionNames = args.getOptionNames();
+        List<String> limit = args.getOptionValues(LIMIT_OPTION);
+        if (!nonOptionArgs.isEmpty()) {
+            if (nonOptionArgs.size() > 1) {
+                System.out.println("Only one source file allowed!");
+                usage();
+            }
+            String fileName = nonOptionArgs.get(0);
+            if (!new File(fileName).exists()) {
+                System.out.printf("Source file '%s' don't exist!%n", fileName);
+                usage();
+            }
+        }
+        if (!optionNames.isEmpty()) {
+            if (!(optionNames.size() == 1 && optionNames.contains(LIMIT_OPTION))) {
+                System.out.println("Only '--limit=n' option is allowed!");
+                usage();
+            }
+            if (limit.size() > 1) {
+                System.out.println("Only one value of '--limit=n' option is allowed!");
+                usage();
+            }
+            String limitValueString = limit.get(0);
+            if (!StringUtils.isNumeric(limitValueString) || Integer.parseInt(limitValueString) <= 0) {
+                System.out.println("In option '--limit=n' n have to be integer >= 1");
+                usage();
+            }
+        }
+    }
+
+    private void usage() {
+        System.out.printf("""
+Usage:
+    java -jar getindata-linserver-*.jar --limit=n fileName
+Where:
+    fileName - optional name of file with text (if missing default '%1$s' is used)
+    --limit - optional limit to length of indexed phrases (if missing it means no limit)
+Examples:
+    java -jar getindata-linserver-*.jar                     ['%1$s' as input and no limit of phrase length] 
+    java -jar getindata-linserver-*.jar  small.txt          ['small.txt' as input and no limit of phrase length] 
+    java -jar getindata-linserver-*.jar --limit=2 small.txt ['small.txt' as input and limit of phrase length is set to 2] 
+                """, DEFAULT_INPUT_FILE);
+        System.exit(1);
     }
 }
