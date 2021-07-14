d804692a8c7018b7ad188c74e21bf3f2e259c060
jslupicki <YwUyezN3>
2021-07-14 06:40:40 +0200

    Multithreded performance test and ignore case in search and index.

--- rust-test/src/main.rs
+++ rust-test/src/main.rs
@@ -1,5 +1,6 @@
-use std::{collections::{HashMap, HashSet}, env::{current_dir}, fs, str};
+use std::{collections::{HashMap, HashSet}, env::current_dir, fs, str, sync::Arc, thread::{self, JoinHandle}};
 use stopwatch::Stopwatch;
+use num_format::{Locale, ToFormattedString};
 
 #[derive(Debug)]
 struct Node<'a> {
@@ -9,35 +10,38 @@ struct Node<'a> {
 }
 
 fn tokenizer(t: &str) -> Vec<&str> {
+    let t_lowercase = to_lowercase(t);
     let mut r = Vec::with_capacity(512);
     let mut li = 0;
     let mut i = 0;
     let mut last_was_whitespace = false;
     for c in t.chars() {
         if c.is_whitespace() != last_was_whitespace && li < i {
-            r.push(&t[li..i]);
+            r.push(&t_lowercase[li..i]);
             li = i;
         }
         last_was_whitespace = c.is_whitespace();
         i += c.len_utf8();
     }
-    r.push(&t[li..]);
+    r.push(&t_lowercase[li..]);
     r
 }
 
+// Warning - this tokenizer can't handle UTF8
 #[allow(dead_code)]
 fn f_tokenizer(t: &str) -> Vec<&str> {
     let mut r = Vec::with_capacity(512);
-    let (li, _) = t.chars().enumerate().fold(
-        (0, false),
-        |(li, last_was_whitespace), (i, c)|
+    let (li, _) = t
+        .chars()
+        .enumerate()
+        .fold((0, false), |(li, last_was_whitespace), (i, c)| {
             if c.is_whitespace() != last_was_whitespace && li < i {
                 r.push(&t[li..i]);
                 (i, c.is_whitespace())
             } else {
                 (li, c.is_whitespace())
-            },
-    );
+            }
+        });
     r.push(&t[li..]);
     r
 }
@@ -54,7 +58,7 @@ fn index(txt: &str) -> Node {
         root = index_line(line, root);
         println!("Indexed line {} from {}", l, line_count);
         l += 1;
-    };
+    }
     root
 }
 
@@ -86,11 +90,73 @@ fn search<'a>(phrase: &'a str, root: &'a Node) -> Option<&'a HashSet<&'a str>> {
     Some(&node.lines)
 }
 
-fn main() {
+fn read_to_string(path: &str) -> &'static str {
+    let source = fs::read_to_string(path).unwrap();
+    Box::leak(Box::new(source))
+}
+
+fn to_lowercase(txt: &str) -> &'static str {
+    let txt_lowercase = txt.to_lowercase();
+    Box::leak(Box::new(txt_lowercase))
+}
+
+fn performance_test() {
     let mut stopwatch = Stopwatch::new();
+    let source_txt = read_to_string("../20_000_mil_podmorskiej_zeglugi.txt");
+    println!("Read source text - {} lines", source_txt.lines().count());
+    stopwatch.start();
+    let indexed_source = index(source_txt);
+    stopwatch.stop();
+    let indexing_took = stopwatch.elapsed();
+    let index_source = Arc::new(indexed_source);
+    let mut handles: Vec<JoinHandle<()>> = Default::default();
+    let how_many_search = 1_000_000;
+    let how_many_threads = 100;
+    stopwatch.restart();
+    for _ in 0..how_many_threads {
+        let local_index_source = index_source.clone();
+        let handle = thread::spawn(move || {
+            let phrase = "Ned Land";
+            for _ in 0..how_many_search {
+                search(phrase, &local_index_source);
+            }
+        });
+        handles.push(handle);
+    }
+    for h in handles {
+        h.join().unwrap();
+    }
+    stopwatch.stop();
+    let phrase = "Ned Land";
+    let lines = search(phrase, &index_source);
+    let search_took = stopwatch.elapsed();
+    println!("Found '{}' in {} lines", phrase, lines.unwrap().len());
+    println!("Indexing took {:?}", indexing_took);
+    println!("Searching took {:?}", search_took);
+    println!(
+        "Perform {}/s searches",
+        ((how_many_search as f64 * how_many_threads as f64/ search_took.as_micros() as f64 * 1_000_000f64) as i128).to_formatted_string(&Locale::en)
+    );
+}
+
+fn main() {
     let txt = "a
 a b
-a b c";
+a b c
+A
+A b
+A b c
+A
+A B
+A b c
+A
+A B
+A B c
+A
+A B
+A B 
+Jerzy Brzęczyszczykiewicz
+";
     let phrases = vec!["a", "b", "c", "a b", "b c", "a b c", " ", "non existent"];
     println!("-------------");
     println!("'{}'", txt);
@@ -102,23 +168,5 @@ a b c";
         println!("'{}' -> {:?}", phrase, lines.unwrap_or(&Default::default()));
     }
     println!("Current dir: {:?}", current_dir().unwrap());
-    let source_txt = fs::read_to_string("../20_000_mil_podmorskiej_zeglugi.txt").unwrap();
-    println!("Read source text - {} lines", source_txt.lines().count());
-    stopwatch.start();
-    let indexed_source = index(source_txt.as_str());
-    stopwatch.stop();
-    let indexing_took = stopwatch.elapsed();
-    let phrase = "Ned Land";
-    stopwatch.restart();
-    let how_many_search = 1_000_000;
-    for _ in 0..how_many_search {
-        search(phrase, &indexed_source);
-    }
-    let lines = search(phrase, &indexed_source);
-    stopwatch.stop();
-    let search_took = stopwatch.elapsed();
-    println!("Found '{}' in {} lines", phrase, lines.unwrap().len());
-    println!("Indexing took {:?}", indexing_took);
-    println!("Searching took {:?}", search_took);
-    println!("Perform {}/s searches", how_many_search as f64 / search_took.as_micros() as f64 * 1_000_000f64);
+    performance_test();
 }
