a8f624ee5fc984d1f7bb5a8f450c561b27dd14a2
jslupicki <YwUyezN3>
2021-08-02 18:43:09 +0200

    Initiate Kotlin version

---
+++ kotlin-test/src/main/kotlin/com/slupicki/getindatalinserverkotlin/GetindataLinserverKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.slupicki.getindatalinserverkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GetindataLinserverKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<GetindataLinserverKotlinApplication>(*args)
+}
